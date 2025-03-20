8021405f008f9b0093c98e1d58ce444209363fe6
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-21 01:15:08 +0530

    i have working on structural directive like *ngIf and ng-template

--- demo/src/app/components/test/test.component.ts
+++ demo/src/app/components/test/test.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,NgTemplateOutlet],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
   name:string="Ritesh Tiwari";
   status:Boolean=true;
+  courses:string[]=["Angular","react","viue"];
   xyz(){
     this.status=!this.status;
   }
