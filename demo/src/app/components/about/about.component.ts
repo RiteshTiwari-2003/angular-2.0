90ec25e22b9574a42d92094ebef3aefa2dd340b7
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-21 18:07:22 +0530

    i have took knowledge about angular pipes like uppercase , lowercase, titlecase, date , slice

--- demo/src/app/components/about/about.component.ts
+++ demo/src/app/components/about/about.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  amount:any=56789;
+  name:string="Ritesh Tiwari";
+  dob:any='1987-17-10';
+  data:any="hi this is dummy data";
 
 }
