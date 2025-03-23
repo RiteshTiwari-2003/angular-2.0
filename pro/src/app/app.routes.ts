600bc9dcb4d1c3cbc9f46ebf1143e510aa13e8c4
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-23 16:42:39 +0530

    i have added one more component page not found which redirect to not found page if you put any other path beside of five route

--- pro/src/app/app.routes.ts
+++ pro/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './components/about/about.component';
 import { ServiceComponent } from './components/service/service.component';
 import { ProductComponent } from './components/product/product.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'service',component:ServiceComponent},
     {path:'product',component:ProductComponent},
-    {path:'contact',component:ContactComponent}
+    {path:'contact',component:ContactComponent},
+    {path:'**',component:NotfoundComponent}
 
 ];
