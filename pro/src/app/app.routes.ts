f7124041ee1d13bb44b9a596f6301cb329bf0266
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-23 14:44:01 +0530

    i have added all route path in app.component.ts

--- pro/src/app/app.routes.ts
+++ pro/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ProductComponent } from './components/product/product.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'service',component:ServiceComponent},
+    {path:'product',component:ProductComponent},
+    {path:'contact',component:ContactComponent}
+
+];
