14a9bc33170c4fe4a351c98cc32b7d2cd944e784
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-24 18:51:03 +0530

    i have known about ngForm and ngModel use in form validation

--- pro/src/app/components/templateform/templateform.component.ts
+++ pro/src/app/components/templateform/templateform.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-templateform',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './templateform.component.html',
   styleUrl: './templateform.component.css'
 })
 export class TemplateformComponent {
+  formData:any={fullName:'',email:'',password:'',mobile:''};
+  formSubmit(){
+    console.log(this.formData);
+  }
 
 }
