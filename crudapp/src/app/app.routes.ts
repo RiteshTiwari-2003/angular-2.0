1c675b6f157a17c470026098b7f9fba457f0a0ce
Ritesh <riteshtiwari3112003@gmail.com>
2025-03-25 17:25:30 +0530

    i have added two more component products and addproduct

--- crudapp/src/app/app.routes.ts
+++ crudapp/src/app/app.routes.ts
@@ -1,6 +1,10 @@
+import { ProductsComponent } from './components/products/products.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AddproductsComponent } from './components/addproducts/addproducts.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'addproduct',component:AddproductsComponent},
 ];
